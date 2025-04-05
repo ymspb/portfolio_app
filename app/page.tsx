58c5cc640551da124cb0e7481d2da50c71d606db
ymspb <185587276+ymspb@users.noreply.github.com>
2025-04-05 22:15:28 +0900

    introduce shadcn/ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,9 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
-    <div className="flex gap-4 mx-4">
-      <div className="w-1/2 flex flex-col gap-1">
-        <div className="bg-green-400">Column Left Row 1</div>
-        <div className="bg-green-400">Column Left Row 2</div>
-        <div className="bg-green-400">Column Left Row 3</div>
-      </div>
-      <div className="w-1/2  flex flex-col gap-1">
-        <div className="bg-blue-500">Column Right Row 1</div>
-        <div className="bg-blue-500">Column Right Row 2</div>
-        <div className="bg-blue-500">Column Right Row 3</div>
-      </div>
+    <div>
+      <Button>Click me</Button>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     content: [
       './app/**/*.{js,ts,jsx,tsx}',
       './public/**/*.{js,ts,jsx,tsx}',
+      "./node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}" 
     ],
     theme: {
       extend: {},
