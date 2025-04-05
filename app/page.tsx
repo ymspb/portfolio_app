e2c557d41c22bf0f86217cc63bf968d222fe8d47
ymspb <185587276+ymspb@users.noreply.github.com>
2025-04-05 21:16:41 +0900

    introduce tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <div></div>
+    <div className="flex gap-4 mx-4">
+      <div className="w-1/2 flex flex-col gap-1">
+        <div className="bg-green-400">Column Left Row 1</div>
+        <div className="bg-green-400">Column Left Row 2</div>
+        <div className="bg-green-400">Column Left Row 3</div>
+      </div>
+      <div className="w-1/2  flex flex-col gap-1">
+        <div className="bg-blue-500">Column Right Row 1</div>
+        <div className="bg-blue-500">Column Right Row 2</div>
+        <div className="bg-blue-500">Column Right Row 3</div>
+      </div>
+    </div>
   )
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+      './app/**/*.{js,ts,jsx,tsx}',
+      './public/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
