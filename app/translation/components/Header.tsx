e1a613b70daf2f95055b576197de08962d1a7961
ymspb <185587276+ymspb@users.noreply.github.com>
2025-04-09 14:33:21 +0900

    extract Header component

---
+++ app/translation/components/Header.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useState } from "react";
+import { ArrowLeftRight } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+
+const Header: React.FC = () => {
+  const [isJPToEN, setIsJPToEN] = useState(true);
+  const getTargetAndResultLang = (isJPToEN: boolean): string =>
+    isJPToEN ? "Japanese" : "English";
+  return (
+    <div className="translation-header flex flex-col md:flex-row h-[48px]">
+      <div className="h-auto w-full md:basis-5/11 text-2xl font-bold p-3 text-red-600">
+        {getTargetAndResultLang(isJPToEN)}
+      </div>
+      <Separator orientation="vertical" className="bg-black" />
+      <div
+        className="w-full md:basis-1/11 flex justify-center items-center"
+        onClick={() => setIsJPToEN((prev) => !prev)}
+      >
+        <Button className="border rounded-xs">
+          <ArrowLeftRight />
+        </Button>
+      </div>
+      <Separator orientation="vertical" className="bg-black" />
+      <div className="w-full md:basis-5/11 text-blue-600 text-2xl font-bold p-3">
+        {getTargetAndResultLang(!isJPToEN)}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
