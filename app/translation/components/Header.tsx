755a6f8d8a625ad78ea162039fa6c62171be070b
ymspb <185587276+ymspb@users.noreply.github.com>
2025-04-10 11:36:07 +0900

    add input area

--- app/translation/components/Header.tsx
+++ app/translation/components/Header.tsx
@@ -10,23 +10,26 @@ const Header: React.FC = () => {
   const getTargetAndResultLang = (isJPToEN: boolean): string =>
     isJPToEN ? "Japanese" : "English";
   return (
-    <div className="translation-header flex flex-col md:flex-row h-[48px]">
-      <div className="h-auto w-full md:basis-5/11 text-2xl font-bold p-3 text-red-600">
-        {getTargetAndResultLang(isJPToEN)}
+    <div>
+      <div className="translation-header flex flex-row h-[48px] mb-3">
+        <div className="h-auto w-full md:basis-5/11 text-2xl font-bold p-3 text-red-600">
+          {getTargetAndResultLang(isJPToEN)}
+        </div>
+        <Separator orientation="vertical" className="bg-black" />
+        <div
+          className="w-full md:basis-1/11 flex justify-center items-center"
+          onClick={() => setIsJPToEN((prev) => !prev)}
+        >
+          <Button className="border rounded-xs">
+            <ArrowLeftRight />
+          </Button>
+        </div>
+        <Separator orientation="vertical" className="bg-black" />
+        <div className="w-full md:basis-5/11 text-blue-600 text-2xl font-bold p-3">
+          {getTargetAndResultLang(!isJPToEN)}
+        </div>
       </div>
-      <Separator orientation="vertical" className="bg-black" />
-      <div
-        className="w-full md:basis-1/11 flex justify-center items-center"
-        onClick={() => setIsJPToEN((prev) => !prev)}
-      >
-        <Button className="border rounded-xs">
-          <ArrowLeftRight />
-        </Button>
-      </div>
-      <Separator orientation="vertical" className="bg-black" />
-      <div className="w-full md:basis-5/11 text-blue-600 text-2xl font-bold p-3">
-        {getTargetAndResultLang(!isJPToEN)}
-      </div>
+      <Separator className="bg-black" />
     </div>
   );
 };
