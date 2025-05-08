b228613f446a310f772023e9589fc1d68c250372
ymspb <185587276+ymspb@users.noreply.github.com>
2025-05-08 17:49:10 +0900

    fix: fix type of params to Promise

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -3,30 +3,30 @@ import { authOptions } from "@/lib/auth";
 import { prisma } from "@/lib/prisma";
 import { notFound, redirect } from "next/navigation";
 
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default async function profile({ params }: PageProps) {
+export default async function profile({ params }: {params: Promise<{ id: string }>}) {
   const { id } = await params;
   const session = await getServerSession(authOptions);
+  console.dir(params)
+  console.log(typeof id)
   if (!session) {
     redirect("user/log_in");
   }
   const user = await prisma.user.findUnique({
     where: {
       id: id,
-    }
+    },
   });
   if (!user) {
     notFound();
   }
   return (
-    <div className="flex flex-col items-center h-screen">
-      <h1 className="text-2xl mt-3">{user.name || "名称未設定"}のプロフィール</h1>
-      <p>{user.email}</p>
+    <div className="grid gird-cols-3 gap-4">
+      <div className="flex flex-col items-center h-screen">
+        <h1 className="text-2xl mt-3">
+          {user.name || "名称未設定"}のプロフィール
+        </h1>
+        <p>{user.email}</p>
+      </div>
     </div>
   );
 }
