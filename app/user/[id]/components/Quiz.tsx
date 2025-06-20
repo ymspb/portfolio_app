18a23ae9b2648c9478f1a7a6f730d1c8f5ccf5d9
ymspb <185587276+ymspb@users.noreply.github.com>
2025-06-20 14:34:07 +0900

    feat: add button to dashboard

--- app/user/[id]/components/Quiz.tsx
+++ app/user/[id]/components/Quiz.tsx
@@ -1,11 +1,16 @@
-'use client'
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 const Quiz = () => {
   return (
-    <Button>クイズに挑戦</Button>
-  )
-}
+    <div className="flex">
+      <Button className="bg-blue-500 text-white hover:bg-blue-600 ">
+        日英クイズ
+      </Button>
+      <Button>クイズに挑戦</Button>
+    </div>
+  );
+};
 
 export default Quiz;
